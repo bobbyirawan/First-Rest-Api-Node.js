445067e2850143536286b65ece5f7859a0ff8cf2
bobbyyyy <lastgame5423@gmail.com>
2020-08-23 11:41:44 +0700

    menambahkan file koneksi

---
+++ koneksi.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+// BUAT KONEKSI DATABASE
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'frestapi'
+})
+
+conn.connect((err) =>{
+    if(err) throw err;
+
+    // JIKA BERHASIL
+    console.log("mysql terkoneksi");
+});
+
+module.exports(conn);
