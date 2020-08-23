855048416492b77d3f4129f55c368070b8ad7797
bobbyyyy <lastgame5423@gmail.com>
2020-08-23 12:27:31 +0700

    update file server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+let routes = require('./routes');
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
