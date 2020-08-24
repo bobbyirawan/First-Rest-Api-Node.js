3468bc22b7f273f52c4a167b349d2257db5b9f76
bobbyyyy <lastgame5423@gmail.com>
2020-08-24 11:25:20 +0700

    create read & insert

--- controller.js
+++ controller.js
@@ -2,7 +2,49 @@
 
 var response = require('./rest');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
     response.ok("Rest Api berjalan", res);
 }
+
+// MENAMPILKAN SEMUA DATA MAHASISWA
+exports.tampilDataMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
+
+// MENAMPILKAN SEMUA DATA MAHASISWA BERDASARKAN ID
+exports.tampilDataMahasiswaBerdarkanId = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+}
+
+// INSERT DATA MAHASISWA
+exports.tambahDataMahasiswa = function (req, res) {
+    let nim = req.body.nim;
+    let nama = req.body.nama;
+    let jurusan = req.body.jurusan;
+    connection.query("INSERT INTO mahasiswa (nim, nama, jurusan) VALUES(?,?,?)", [nim, nama, jurusan], function (error, rows, fileds) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok("BERHASIL MENAMBAHKAN DATA", res);
+        }
+    });
+}
+
+// notes : 
+// 1. jika ingin ambil data dari parameter gunakan params. example : let id = req.params.id
+// 2. jika ingin ambil data dari body atau dengan method post gunakan body. example : let nama = req.body.nama
--- routes.js
+++ routes.js
@@ -4,4 +4,7 @@ module.exports = function(app) {
     var jsonku = require('./controller');
 
     app.route('/').get(jsonku.index);
+    app.route('/mahasiswa').get(jsonku.tampilDataMahasiswa);
+    app.route('/mahasiswa/:id').get(jsonku.tampilDataMahasiswaBerdarkanId);
+    app.route('/tambah').post(jsonku.tambahDataMahasiswa);
 }
