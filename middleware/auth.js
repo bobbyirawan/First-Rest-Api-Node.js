a47d176841f4a78f50b6f6a664fe2d269bc12834
bobbyyyy <lastgame5423@gmail.com>
2020-08-28 21:59:53 +0700

    login generate token berhasil

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,10 @@
-let connection  = require('../koneksi')
-let mysql = require('mysql')
-let md5 = require('MD5')
-let response = require('../res')
-let jwt = require('jsonwebtoken')
-let config = require('../config/secret')
-let ip = require('ip')
+let connection  = require('../koneksi');
+let mysql = require('mysql');
+let md5 = require('MD5');
+let response = require('../res');
+let jwt = require('jsonwebtoken');
+let config = require('../config/secret');
+let ip = require('ip');
 
 // CONTROLLER UNTUK REGISTER
 exports.registrasi = function(req, res) {
@@ -40,4 +40,62 @@ exports.registrasi = function(req, res) {
             }
         }
     })
+}
+
+// CONTROLLER UNTUK LOGIN
+exports.login = function(req, res) {
+    let post = {
+        password : req.body.password,
+        email: req.body.email
+    }
+
+    let query = 'SELECT * FROM ?? WHERE ?? = ? AND ?? = ?'
+    let table = ['users', 'password', md5(post.password), "email", post.email]
+
+    // FORMAT MYSQL
+    query = mysql.format(query, table)
+
+    connection.query(query, function(error, rows){
+        if(error) {
+            console.log(error)
+        }else {
+            if(rows.length == 1) {
+                let token = jwt.sign({rows}, config.secret, {
+                    expiresIn: 1440
+                });
+                id_user = rows[0].id
+
+                let data = {
+                    id_user: id_user,
+                    access_token: token,
+                    ip_address: ip.address()
+                }
+
+                let query = "INSERT INTO ?? SET ?";
+                let table = ["akses_token"];
+
+                query = mysql.format(query, table);
+
+                connection.query(query, data, function(error, rows) {
+                    if(error){
+                        console.log(error)
+                    }else {
+                        res.json({
+                            succes: true,
+                            message: "Generate Token Succes",
+                            token: token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            }else {
+                 res.json({
+                     error: true,
+                     message : "Email atau Password Salah!!"
+                 });
+            }
+        }
+    });
+
+
 }
--- middleware/index-auth.js
+++ middleware/index-auth.js
@@ -3,4 +3,5 @@ const auth = require('./auth');
 const router = express.Router();
 
 router.post('/api/v1/register', auth.registrasi);
+router.post('/api/v1/login',auth.login);
 module.exports = router;
